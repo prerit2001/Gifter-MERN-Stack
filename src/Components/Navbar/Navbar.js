93a3e77c481ea37306dea810dc69fb8a731b95e9
Prerit Kumar Jha <preritkrjha@gmail.com>
2020-11-15 14:32:26 +0530

    First Commit - Navbar

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,84 @@
+import React, {useState, useEffect} from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { Nav, NavbarContainer, NavLogo, NavIcon, SideIcon, NavMenu, NavItems, NavLinks, NavBtnItem, NavBtnLink } from './Navbar.element';
+import { IconContext } from 'react-icons/lib'
+import { Button } from '../../Styled-Global'
+
+const Navbar = () => {
+
+    const [click,setClick] = useState(false);
+    const [button,setButton] = useState(true);
+
+    const handleclick = () => setClick(!click);
+
+    const showButton = () => {
+      if(window.innerWidth<960){
+        setButton(false);
+      }
+      else{
+        setButton(true);
+      }
+    }
+    
+    useEffect(() => {
+      showButton();
+    }, [])
+
+    window.addEventListener('resize',showButton);
+
+    return (
+        <>
+          <IconContext.Provider value={{color: '#fff'}}>
+            <Nav>
+              
+              <NavbarContainer> 
+                 
+                  <NavLogo to="/">
+                      <NavIcon />
+                      GIFTER
+                  </NavLogo>
+
+                  <SideIcon onClick={handleclick}>
+                    {click ? <FaTimes /> : <FaBars/>}
+                  </SideIcon>
+
+                  <NavMenu onClick={handleclick} click={click}>
+                      <NavItems>
+                        <NavLinks to='/'> Home </NavLinks>
+                      </NavItems>
+            
+                      <NavItems>
+                        <NavLinks to='/services'> Services </NavLinks>
+                      </NavItems>
+                
+                      <NavItems>
+                        <NavLinks to='/products'> Products </NavLinks>
+                      </NavItems>
+                 
+                      <NavItems>
+                        <NavLinks to='/about'> About </NavLinks>
+                      </NavItems>
+
+                      <NavBtnItem>
+                        {
+                          button ? 
+                            (<NavBtnLink to="/sign-up">
+                                <Button primary>Sign - Up</Button>
+                            </NavBtnLink>) 
+                          :
+                            (<NavBtnLink to="/sign-up">
+                                <Button fontbig primary>Sign - Up</Button>
+                            </NavBtnLink>)
+                        }
+                      </NavBtnItem>
+                  </NavMenu>
+
+              </NavbarContainer>
+
+            </Nav>
+          </IconContext.Provider>
+        </>
+    )
+}
+
+export default Navbar
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+var firebaseConfig = {
+    apiKey: "AIzaSyCQ5tLN031eIliDqT7X7x4CR1nz_VXoZmg",
+    authDomain: "gifter-af699.firebaseapp.com",
+    databaseURL: "https://gifter-af699.firebaseio.com",
+    projectId: "gifter-af699",
+    storageBucket: "gifter-af699.appspot.com",
+    messagingSenderId: "860483920458",
+    appId: "1:860483920458:web:03905d036fe981ed3cbcb3",
+    measurementId: "G-M86ZXCRYXN"
+  };
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render( <App /> , document.getElementById('root') );
+
+
